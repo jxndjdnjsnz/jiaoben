980c624feab93f187c4205c72594fc7e16a0955a
jxndjdnjsnz <jdbjdbsj@qq.com>
2026-08-14 13:42:45 +0800

    Add files via upload

--- JBSpecial script for chasing the wind.lua
+++ JBSpecial script for chasing the wind.lua
@@ -160,31 +160,7 @@ advancedCategory:Button({
 local dynamicTab = mainWindow:Tab({ Title = "通用" })
 local dynamicCategory = dynamicTab:Category({ Title = "全游戏通用", IconName = "users" })
 
-dynamicCategory:Button({
-    Text = "飞行",
-    Icon = "play",
-    Tooltip = "已开启飞行",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
-    end
-)}
-dynamicCategory:Button({
-    Text = "透视",
-    Icon = "play",
-    Tooltip = "已开启透视",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ZIONPCE/raw/refs/heads/main/ESP.lua"))()
-    end
-)}
 
-dynamicCategory:Button({
-    Text = "自瞄",
-    Icon = "play",
-    Tooltip = "已开启自瞄",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ke9460394-dot/ugik/refs/heads/main/%E4%B8%81%E4%B8%81%20%E6%B1%89%E5%8C%96%E8%87%AA%E7%9E%84.txt"))()
-    end
-)}
 basicTab:Paragraph({
     Title = "关于 WasJB",
     Desc = "专门做Twisted X 0.15,作者是jxndjdnjsnz",
